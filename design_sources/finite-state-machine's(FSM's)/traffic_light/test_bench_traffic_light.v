77acdc20cb69611e9affdc8bead24383cb72989e
Praveen <67917708+lone-lobo@users.noreply.github.com>
2021-02-07 12:16:35 +0530

    updated

--- design_sources/finite-state-machine's(FSM's)/traffic_light/test_bench_traffic_light.v
+++ design_sources/finite-state-machine's(FSM's)/traffic_light/test_bench_traffic_light.v
@@ -1,34 +1,34 @@
 module test_traffic_controller;
-  reg [1:0]cur_state;
   reg res_n;
   reg clk;
   reg en;
   wire [3:0]o;
   wire [1:0]next_state;
+  
+  //initialization of test bench signals
   initial
     begin
-      cur_state <= 2'b00;
       res_n <= 1'b1;
       en <= 1'b0;
       clk <= 1'b0;
     end
+  
+  //generate periodic clock signal
   always #1 clk <= ~clk;
-  //d_ff d_ff_1(.q(q),.reset_n(reset_n),.clk(clk),.en(en),.o(o));
-  traffic_controller fsm_1(.res_n(res_n),.cur_state(cur_state),.next_state(next_state),.clk(clk),.en(en),.o(o));
+  
+  //module instantiation
+  traffic_controller fsm_1(.res_n(res_n),.next_state(next_state),.clk(clk),.en(en),.o(o));
+  
   initial begin
-  $dumpfile("dump.vcd");
-  $dumpvars(1);
-  #1 en<=1'b0;    
-     cur_state <= 2'b01;
+  $dumpfile("dump.vcd");                                                                    //dump variables for waveform generation if -> <eda playground>
+  $dumpvars(1);                                                                             //else -> comment the two lines 
+  #1 en <= 1'b0;    
   #5 res_n <= 1'b0;
   #1 en <= 1'b1;
   #1 res_n <= 1'b1;
-  //#5 q<=1'b0;
-  //#15 cur_state <= 2'b01;
-  //#10 cur_state <= 2'b10;
-  //#6  cur_state <= 2'b00;
-  #15 res_n <= 1'b0;
-  #5 en <= 1'b0;
-  #40  $finish;
+  #35 en <= 1'b0;
+  #10 res_n <= 1'b0;
+  #10  $finish;
   end
+  
 endmodule
