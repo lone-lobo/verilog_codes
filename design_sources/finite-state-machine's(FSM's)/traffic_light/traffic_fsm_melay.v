e123efdc840aa24f32d680ad776200267ac5a4d4
Praveen <67917708+lone-lobo@users.noreply.github.com>
2021-02-10 23:59:02 +0530

    update the logic once more

--- design_sources/finite-state-machine's(FSM's)/traffic_light/traffic_fsm_melay.v
+++ design_sources/finite-state-machine's(FSM's)/traffic_light/traffic_fsm_melay.v
@@ -16,77 +16,111 @@ module traffic_controller_melay(res_n,in,clk,en,o);
       cur_state <= s_0;
       count <= 4'b0000;
     end
-    else cur_state <= next_state;
+    //else cur_state <= next_state;
   end
-  always @(cur_state or in or count) begin
+  always @(cur_state or in ) begin
       if(res_n == 1'b1 && en == 1'b1) begin
         case(cur_state)
           s_0:begin
             $display("%t s0_cur->%b s0_next->%b",$time,cur_state,next_state);
+           // #10 o = 4'b1000;
              if(in == s_1) begin
-               next_state = s_2;
+               cur_state = s_1;
                o=4'b0100;
             end
             else if(in == s_2) begin
               o = 4'b010;
-               next_state = s_0;
+               cur_state = s_2;
             end
-            else if(count == 4'b1010) begin
-              count = 4'b0000;
-              next_state = s_1;
-            end
-             else  begin
-               o = 4'b1000;
-               next_state = s_0;
-               count = count+1;
-               $display ("%t s_0->%0h",$time,count);
-            end
+            else begin
+    //          if(count == 4'b1010) begin
+     //         count = 4'b0000;
+     //         cur_state = s_1;
+     //       end
+     //        else  begin
+     //          o = 4'b1000;
+     //          cur_state = s_0;
+      //         count = count+1;
+      //         $display ("%t s_0->%0h",$time,count);
+      //      end
+              while( count != 10) begin
+                o = 4'b1000;
+                count = count + 1;
+                $display("%t %d",$time,count);
+              end
+              //o = 4'b0100;
+              count = 0;
+              cur_state = s_1;
+              $display("%t s_0-> %b",$time,cur_state);
+          end
           end
           s_1: begin
             $display("%t s1_cur->%b s1_next->%b",$time,cur_state,next_state);
            if(in == s_2) begin
               o = 4'b0010;
-              next_state = s_3;
+              cur_state = s_2;
             end
             else if(in == s_0) begin
               o = 4'b1000;
-              next_state = s_0;
+              cur_state = s_0;
             end
-            else if(count==4'b0010) begin
-              count = 4'b0000;
-              next_state = s_2;
-            end
-             else begin
-              o= 4'b1000;
-              next_state = s_1;
-              count = count+1;
-               $display ("%t s_1->%0h",$time,count);
+            else begin
+   //           if(count==4'b0010) begin
+    //          count = 4'b0000;
+     //         cur_state = s_2;
+      //      end
+      //       else begin
+      //        o= 4'b1000;
+      //        cur_state = s_1;
+      //        count = count+1;
+      //         $display ("%t s_1->%0h",$time,count);
+      //     end
+              while( count != 2) begin
+                o = 4'b0100;
+                count = count + 1;
+                $display("%t %d",$time,count);
+              end
+              //o = 4'b0100;
+              count = 0;
+              cur_state = s_2;
+              $display("%t s_0-> %b",$time,cur_state);
             end
           end
           s_2: begin
             $display("%t s2_cur->%b s2_next->%b",$time,cur_state,next_state);
            if(in == s_0) begin
               o = 4'b1000;
-             next_state = s_1;
+             cur_state = s_0;
             end
             else if(in == s_1) begin
-              o = 4'b0100;
-              next_state = s_2;
+              //o = 4'b0100;
+              cur_state = s_1;
             end
-            else if(count==4'b1000) begin
-              count = 4'b0000;
-              next_state = s_0;
-            end
-             else  begin
-               o =4'b1000;
-                next_state = s_2;
-               count = count+1;
-               $display ("%t s_2->%0h",$time,count);
-            end
+            else begin
+    //          if(count==4'b1000) begin
+     //         count = 4'b0000;
+     //         cur_state = s_0;
+    //       end
+    //         else  begin
+     //          o =4'b1000;
+      //          cur_state = s_2;
+      //         count = count+1;
+     //          $display ("%t s_2->%0h",$time,count);
+     //       end
+              while( count != 8) begin
+                o = 4'b0010;
+                count = count + 1;
+                $display("%t %d",$time,count);
+              end
+              //o = 4'b0100;
+              count = 0;
+              cur_state = s_1;
+              $display("%t s_0-> %b",$time,cur_state); 
+          end
           end
           default:begin
             o = 4'b0111;
-            next_state = s_0;
+            cur_state = s_0;
           end 
     endcase
   end
