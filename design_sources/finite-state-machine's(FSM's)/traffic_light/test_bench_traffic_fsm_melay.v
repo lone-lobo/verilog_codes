d3264bf1a1fdc578931af6c78a7380d1cd0293af
Praveen <67917708+lone-lobo@users.noreply.github.com>
2021-02-10 23:59:39 +0530

    update

--- design_sources/finite-state-machine's(FSM's)/traffic_light/test_bench_traffic_fsm_melay.v
+++ design_sources/finite-state-machine's(FSM's)/traffic_light/test_bench_traffic_fsm_melay.v
@@ -2,14 +2,16 @@ module test_traffic_controller;
   reg res_n;
   reg clk;
   reg en;
+  //reg [1:0]next_state;
   //reg [1:0]cur_state;
   reg [1:0]in;
   wire [3:0]o;
+  //wire [1:0]next_state;
  
   initial
     begin
       res_n <= 1'b1;
-      in <= 2'b00;
+      in <= 2'b01;
       en <= 1'b1;
       clk <= 1'b0;
     end
@@ -19,7 +21,7 @@ module test_traffic_controller;
   $dumpfile("dump.vcd");
   $dumpvars(1);
   #1 en=1'b1;    
-  #10 res_n = 1'b1;
+  //#2 res_n = 1'b0;
   #10  in = 2'b00;
   #2 en = 1'b1;
   #2 res_n = 1'b1;
