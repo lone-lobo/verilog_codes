f1c6ae43a8072428edbe9e43dec42c8025a5c027
Praveen <67917708+lone-lobo@users.noreply.github.com>
2021-02-11 23:47:44 +0530

    update the logic

--- design_sources/finite-state-machine's(FSM's)/traffic_light/traffic_fsm_melay.v
+++ design_sources/finite-state-machine's(FSM's)/traffic_light/traffic_fsm_melay.v
@@ -16,21 +16,21 @@ module traffic_controller_melay(res_n,in,clk,en,o);
       cur_state <= s_0;
       count <= 4'b0000;
     end
-    //else cur_state <= next_state;
+    else cur_state <= next_state;
   end
-  always @(cur_state or in ) begin
+  always @(cur_state or in) begin
       if(res_n == 1'b1 && en == 1'b1) begin
-        case(cur_state)
+        case(next_state)
           s_0:begin
             $display("%t s0_cur->%b s0_next->%b",$time,cur_state,next_state);
            // #10 o = 4'b1000;
-             if(in == s_1) begin
-               cur_state = s_1;
+            if (in == s_1) begin
+               next_state = s_1;
                o=4'b0100;
             end
             else if(in == s_2) begin
               o = 4'b010;
-               cur_state = s_2;
+              #2 next_state = s_2;
             end
             else begin
     //          if(count == 4'b1010) begin
@@ -43,26 +43,26 @@ module traffic_controller_melay(res_n,in,clk,en,o);
       //         count = count+1;
       //         $display ("%t s_0->%0h",$time,count);
       //      end
-              while( count != 10) begin
+              while( count <= 10) begin
                 o = 4'b1000;
-                count = count + 1;
-                $display("%t %d",$time,count);
+                count = @(clk)count + 1;
+                $display("%t %d %d",$time,count,o);
               end
-              //o = 4'b0100;
+              o = 4'b0100;
               count = 0;
-              cur_state = s_1;
-              $display("%t s_0-> %b",$time,cur_state);
+               next_state = s_1;
+              $display("%t s_0-> %b  %d",$time,cur_state,o);
           end
           end
           s_1: begin
             $display("%t s1_cur->%b s1_next->%b",$time,cur_state,next_state);
            if(in == s_2) begin
               o = 4'b0010;
-              cur_state = s_2;
+               next_state = s_2;
             end
             else if(in == s_0) begin
               o = 4'b1000;
-              cur_state = s_0;
+              next_state = s_0;
             end
             else begin
    //           if(count==4'b0010) begin
@@ -75,26 +75,26 @@ module traffic_controller_melay(res_n,in,clk,en,o);
       //        count = count+1;
       //         $display ("%t s_1->%0h",$time,count);
       //     end
-              while( count != 2) begin
+              while( count < 2) begin
                 o = 4'b0100;
-                count = count + 1;
-                $display("%t %d",$time,count);
+                count = @(posedge clk )count + 1;
+                $display("%t %d %d",$time,count,o);
               end
-              //o = 4'b0100;
+              o = 4'b0010;
               count = 0;
-              cur_state = s_2;
-              $display("%t s_0-> %b",$time,cur_state);
+               next_state = s_2;
+              $display("%t s_1-> %b  %d",$time,cur_state,o);
             end
           end
           s_2: begin
             $display("%t s2_cur->%b s2_next->%b",$time,cur_state,next_state);
            if(in == s_0) begin
               o = 4'b1000;
-             cur_state = s_0;
+             next_state = s_0;
             end
             else if(in == s_1) begin
-              //o = 4'b0100;
-              cur_state = s_1;
+              o = 4'b0100;
+              next_state = s_1;
             end
             else begin
     //          if(count==4'b1000) begin
@@ -107,20 +107,20 @@ module traffic_controller_melay(res_n,in,clk,en,o);
       //         count = count+1;
      //          $display ("%t s_2->%0h",$time,count);
      //       end
-              while( count != 8) begin
-                o = 4'b0010;
-                count = count + 1;
-                $display("%t %d",$time,count);
+              while( count <  5) begin
+                o = 4'b0100;
+                count = @(posedge clk)count + 1;
+                $display("%t %d %d",$time,count,o);
               end
-              //o = 4'b0100;
+              o = 4'b1000;
               count = 0;
-              cur_state = s_1;
-              $display("%t s_0-> %b",$time,cur_state); 
+              next_state = s_0;
+              $display("%t s_2-> %b %d",$time,cur_state,o); 
           end
           end
           default:begin
             o = 4'b0111;
-            cur_state = s_0;
+            next_state = s_0;
           end 
     endcase
   end
