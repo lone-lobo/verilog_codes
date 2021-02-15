86846e55c30a367a641814832208a326905b0413
Praveen <67917708+lone-lobo@users.noreply.github.com>
2021-02-15 23:40:22 +0530

    updated

---
+++ Test_benches/test_bench_vedic_pure_comb_2x2.v
@@ -0,0 +1,26 @@
+module test_bench_vedic_2x2;
+  reg [1:0]mul_1;
+  reg [1:0]mul_2;
+  wire [3:0] product;
+  
+  initial begin
+    mul_1=2'b00;
+    mul_2=2'b00;
+  end
+  
+  vedic_2x2 vm1(.mul_1(mul_1),.mul_2(mul_2),.product(product));
+ 
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(1);
+    #2 mul_1 <= 2'b01;
+    #2 mul_2 <= 2'b01;
+    #2 mul_1 <= 2'b10;
+    #2 mul_2 <= 2'b10;
+    #2 mul_1 <= 2'b00;
+    #2 mul_1 <= 2'b11;
+    #2 mul_2 <= 2'b11;
+    #2 mul_2 <= 2'b00;
+    #2 $finish;
+  end
+endmodule
